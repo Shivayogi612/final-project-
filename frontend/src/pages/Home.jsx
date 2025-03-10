d7c4afe7c3374e7cb3e52f5d447ce8f63b73f85d
Shivayogi612 <shivayogi310@gmail.com>
2025-03-10 11:36:38 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,15 +3,12 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import Bestseller from '../components/Bestseller'
 import OurPolicy from '../components/OurPolicy'
-import NewsletterBox from '../components/NewsletterBox'
-import DiscountVideo from '../components/Discountvideo'
 
 const Home = () => {
   return (
     <div>
         <Hero/>
         <LatestCollection/>
-        <DiscountVideo/>
         <Bestseller/>
         <OurPolicy/>
     </div>
